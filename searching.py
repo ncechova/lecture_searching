5a8948af1d6a51f4d3dbc6d96433e97041e6ca27
ncechova <256971@vutbr.cz>
2024-04-10 10:09:07 +0200

    hihi

--- searching.py
+++ searching.py
@@ -1,5 +1,5 @@
 import os
-
+import json
 # get current working directory path
 cwd_path = os.getcwd()
 
@@ -12,11 +12,41 @@ def read_data(file_name, field):
     :return: (list, string),
     """
     file_path = os.path.join(cwd_path, file_name)
+    r'"V:\BPC-PRG\lecture09\lecture_searching\sequential.json"'
+    with open(file_name) as file:
+        data = json.load(file)
+    if field not in data.keys():
+        return None
+    return data[field]
+
+def linear_search(sequential_data, wanted_number):
+    indexes = []
+    for pozice, number in enumerate(sequential_data):
+        if number == wanted_number:
+            indexes.append(pozice)
+    searched = {}
+    searched["positions"] = indexes
+    searched["count"] = len(indexes)
+    return searched
+
+def pattern_search(sequential_data, wanted_pattern):
+    n = len(sequential_data)
+    m = len(wanted_pattern)
+    positions = set()
+    for i in range(n - (m - 1)):
+        subseq = sequential_data[i:i+m]
+        if subseq == wanted_pattern:
+            positions.add(i)
+    return positions
 
 
 def main():
-    pass
+    sequential_data = read_data("sequential.json", "dna_sequence")
+    # number_in_sequence = linear_search(sequential_data, 0)
+    print(pattern_search(sequential_data, "ATA"))
+    return sequential_data
+
 
 
 if __name__ == '__main__':
-    main()
+    print(main())
